cc18529464c6427c491ea1c73ee2188ed6c8950b
System Administrator <root@Mac-mini.hitronhub.home>
2020-03-20 16:25:03 -0400

    New software added to automated mac config

--- configure.sh
+++ configure.sh
@@ -2,4 +2,15 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 // Install Software Using brew
-brew install hugo
+brew install cask
+
+brew install git
+
+brew cask install visual-studio-code
+
+brew install htop
+
+brew install zsh
+
+brew cask install iterm2
+
