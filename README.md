35353364804dfc65f8f96ab1080107f5fbfab592
Chun Wu <the.chun.wu@gmail.com>
2020-11-13 13:23:01 -0500

    Cleaned up configuration script, added ReadMe/License and updated software list.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Automated Mac Config  
+
+This bash script will automate the configuration for any Mac User with a developer focus. It uses a combination of homebrew, mas-cli (Mac App Store CLI), cask,  and manually downloads for undisclosed apps that are not in aformentioned platforms.
+
+## Foreword
+
+This is an opinionated way to configure a mac.
+- For any apps that are not in app store that are gui based we use brew cask to install the application.
+- For all command line interfaces we use brew to install the application.
+- For any applications found in the app store we use the mas-cli to install the application.
+
+## Q/A
+
+Why we use MAS and not the App Store directly? The mas-cli provides the ability to uninstall and easier management of all applications installed on machine.
+
+Why MAS and not brew? For applications that exist in both brew and the App Store, we will provide the App Store higher order of importance for application updates which App Store will take care of natively.
+
+Why do we a brew add-on? Run brew cu and you will out how easy it is to manage updates and upgrades.
+
+## Authors
+
+Repository managed by [Chun Wu](https://github.com/cpwu)
--- configure.sh
+++ configure.sh
@@ -1,16 +1,30 @@
-// Install Homebrew
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-// Install Software Using brew
+# Install Software Using brew
 brew install cask
 
-brew install git
 
+# Visual Studio Code (https://www.visualstudio.com/vs/visual-studio-mac/) / (https://formulae.brew.sh/cask/visual-studio#default)
 brew cask install visual-studio-code
+# Spectacle (https://www.spectacleapp.com) / (https://formulae.brew.sh/cask/spectacle)
+brew cask install spectacle
+# Microsoft Teams (https://www.microsoft.com/en-us/microsoft-365/microsoft-teams/download-app) / (https://formulae.brew.sh/cask/microsoft-teams#default)
 
-brew install htop
+# Mac App Store CLI (https://github.com/mas-cli/mas) / (https://formulae.brew.sh/formula/mas#default)
+brew install mas
+# Terraform CLI (https://www.terraform.io) / (https://formulae.brew.sh/formula/terraform#default)
+brew install terraform 
+# AWS CLI (https://aws.amazon.com/cli/) / (https://formulae.brew.sh/formula/awscli#default)
+brew install awscli
+# Azure CLI (https://docs.microsoft.com/cli/azure/overview) / (https://formulae.brew.sh/formula/azure-cli#default)
+brew install azure-cli
 
-brew install zsh
+# brew-cask-upgrade (https://github.com/buo/homebrew-cask-upgrade)
+brew tap buo/cask-upgrade
 
-brew cask install iterm2
-
+# Installation Using MAS CLI & Application ID
+# Xcode
+mas install 497799835
+# Slack
+mas install 803453959
