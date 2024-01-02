c10e86468cea25cd74a6daca5ae356279b205c37
Chun Wu <the.chun.wu@gmail.com>
2024-01-01 22:05:29 -0800

    Software update for 01/01/2024

--- configure.sh
+++ configure.sh
@@ -12,34 +12,28 @@ brew install cask
 brew install --cask visual-studio-code
 # Spectacle (https://www.spectacleapp.com) / (https://formulae.brew.sh/cask/spectacle)
 brew install --cask spectacle
-# Microsoft Teams (https://www.microsoft.com/en-us/microsoft-365/microsoft-teams/download-app) / (https://formulae.brew.sh/cask/microsoft-teams#default)
-brew install --cask microsoft-teams
+# # Microsoft Teams (https://www.microsoft.com/en-us/microsoft-365/microsoft-teams/download-app) / (https://formulae.brew.sh/cask/microsoft-teams#default)
+# brew install --cask microsoft-teams
 # Sourcetree (https://www.sourcetreeapp.com) / (https://formulae.brew.sh/cask/sourcetree)
 brew install --cask sourcetree
-# OBS (https://obsproject.com) / (https://formulae.brew.sh/cask/obs)
-brew install --cask obs
-# Zoom US (https://www.zoom.us/) / (https://formulae.brew.sh/cask/zoomus)
-brew install --cask zoomus
-# Discord (https://discord.com) / (https://formulae.brew.sh/cask/discord)
-brew install --cask discord
-# Menu Meters (https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/) / (https://formulae.brew.sh/cask/menumeters)
-brew install --cask menumeters
+# # Discord (https://discord.com) / (https://formulae.brew.sh/cask/discord)
+# brew install --cask discord
+# # Menu Meters (https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/) / (https://formulae.brew.sh/cask/menumeters)
+# brew install --cask menumeters
 # Docker Desktop (https://www.docker.com/products/docker-desktop) / (https://formulae.brew.sh/cask/docker#default)
 brew install --cask docker
 # Google Chrome (https://www.google.com/chrome/) / (https://formulae.brew.sh/cask/google-chrome#default)
 brew install --cask google-chrome
 # Slack (https://slack.com/) / (https://formulae.brew.sh/cask/slack) 
 brew install --cask slack
-# Box (https://www.box.com/drive) / (https://formulae.brew.sh/cask/box-drive#default)
-brew install --cask box-drive
-# Webex (https://www.webex.com/) / (https://formulae.brew.sh/cask/webex-meetings)
-brew install --cask webex-meetings
-# MySQL Workbench (https://www.mysql.com/products/workbench/)/(https://formulae.brew.sh/cask/mysqlworkbench)
-brew install --cask mysqlworkbench
-# Adobe Reader (https://formulae.brew.sh/cask/adobe-acrobat-reader) / https://acrobat.adobe.com/us/en/acrobat/pdf-reader.html
-brew install --cask adobe-acrobat-reader
-# Transmission (https://transmissionbt.com/)/(https://formulae.brew.sh/cask/transmission)
-brew install --cask transmission
+# # Box (https://www.box.com/drive) / (https://formulae.brew.sh/cask/box-drive#default)
+# brew install --cask box-drive
+# # Webex (https://www.webex.com/) / (https://formulae.brew.sh/cask/webex-meetings)
+# brew install --cask webex-meetings
+# # MySQL Workbench (https://www.mysql.com/products/workbench/)/(https://formulae.brew.sh/cask/mysqlworkbench)
+# brew install --cask mysqlworkbench
+# # Adobe Reader (https://formulae.brew.sh/cask/adobe-acrobat-reader) / https://acrobat.adobe.com/us/en/acrobat/pdf-reader.html
+# brew install --cask adobe-acrobat-reader
 
 # Mac App Store CLI (https://github.com/mas-cli/mas) / (https://formulae.brew.sh/formula/mas#default)
 brew install mas
