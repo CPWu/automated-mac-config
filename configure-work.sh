546f54ff01989e58c3c223c3a763b7a642b1f772
Chun Wu <chunw@nvidia.com>
2025-10-24 17:55:18 -0700

    Updates from 10/24/2025

--- configure-work.sh
+++ configure-work.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# #!/bin/bash
+# # Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install Apple Command Line Tools
 xcode-select --install
@@ -24,10 +24,24 @@ brew install --cask discord
 brew install --cask menumeters
 # Docker Desktop (https://www.docker.com/products/docker-desktop) / (https://formulae.brew.sh/cask/docker#default)
 brew install --cask docker
+# # OpenAI Atlas (https://formulae.brew.sh/cask/chatgpt-atlas)
+brew install --cask chatgpt-atlas
+# Amazon Chime (https://formulae.brew.sh/cask/amazon-chime#default)
+brew install --cask amazon-chime
+# Balena Etcher (https://formulae.brew.sh/cask/balenaetcher)
+brew install --cask balenaetcher
+# Elgato Camera Hub (https://formulae.brew.sh/cask/elgato-camera-hub#default)
+brew install --cask elgato-camera-hub
+# VLC (https://formulae.brew.sh/cask/vlc#default)
+brew install --cask vlc
+# Zoom (https://formulae.brew.sh/cask/zoom#default)
+brew install --cask zoom
+# Prusa Slicer (https://formulae.brew.sh/cask/prusaslicer)
+brew install --cask prusaslicer
 
-# CLIs
-# Mac App Store CLI (https://github.com/mas-cli/mas) / (https://formulae.brew.sh/formula/mas#default)
-brew install mas
+# # CLIs
+# # Mac App Store CLI (https://github.com/mas-cli/mas) / (https://formulae.brew.sh/formula/mas#default)
+# brew install mas
 # Terraform Version Manager (https://github.com/tfutils/tfenv) / (https://formulae.brew.sh/formula/tfenv)
 brew install tfenv
 # AWS CLI (https://aws.amazon.com/cli/) / (https://formulae.brew.sh/formula/awscli#default)
@@ -36,10 +50,14 @@ brew install awscli
 brew install terragrunt
 # Kubernetes-cli (https://kubernetes.io/) / (https://formulae.brew.sh/formula/kubernetes-cli)
 brew install kubernetes-cli
-# brew-cask-upgrade (https://github.com/buo/homebrew-cask-upgrade)
-brew tap buo/cask-upgrade
+# Terraform-Docs (https://formulae.brew.sh/formula/terraform-docs)``
+brew install terraform-docs
+# Python package installer (https://formulae.brew.sh/formula/uv)
+brew install uv
 # # Golang (https://golang.org) / (https://formulae.brew.sh/formula/go#default)
 brew install go
+# brew-cask-upgrade (https://github.com/buo/homebrew-cask-upgrade)
+brew tap buo/cask-upgrade
 
 # Check for Updates:
 brew cu
